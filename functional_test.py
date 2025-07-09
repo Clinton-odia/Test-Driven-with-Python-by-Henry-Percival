414983cdfc074252e68033f7066c34a3f3d6d0b6
Clinton <odiamehiclinton@gmail.com>
2025-07-09 16:08:11 +0100

    app setup and chapter 1 conclusion

--- functional_test.py
+++ functional_test.py
@@ -1,16 +1,24 @@
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.common.by import By
+# import time
+
+# # ✅ Updated path
+# service = Service(
+#     executable_path="C:/Users/USER/Downloads/chromedriver-win64/chromedriver.exe"
+# )
+# driver = webdriver.Chrome(service=service)
+
+# driver.get("https://www.google.com")
+# print("Page title is:", driver.title)
+
+# time.sleep(3)
+# driver.quit()
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-import time
 
-# ✅ Updated path
-service = Service(
-    executable_path="C:/Users/USER/Downloads/chromedriver-win64/chromedriver.exe"
-)
-driver = webdriver.Chrome(service=service)
+browser = webdriver.Chrome()
+browser.get("http://localhost:8000")
 
-driver.get("https://www.google.com")
-print("Page title is:", driver.title)
-
-time.sleep(3)
-driver.quit()
+assert "Congratulations" in browser.title
+print("ok")
