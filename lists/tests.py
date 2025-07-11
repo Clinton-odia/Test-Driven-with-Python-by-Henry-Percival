b6ee66f90064664413955b085497ce48cf5ecb31
Clinton <odiamehiclinton@gmail.com>
2025-07-11 18:48:23 +0100

    url config | map urls to homepage

--- lists/tests.py
+++ lists/tests.py
@@ -9,15 +9,33 @@ from lists.views import home_page
 #     def test_bad_math(self):
 #         self.assertEqual(1 + 1, 3)
 
+# Old test versions
+
+# class HomePageTest(TestCase):
+#     """Test the home page"""
+
+#     def test_home_page_returns_correct_html(self):
+#         request = HttpRequest()
+#         response = home_page(request)
+
+#         html = response.content.decode("utf8")
+#         self.assertIn("<title>To-Do lists</title>", html)
+#         self.assertTrue(html.startswith("<html>"))
+#         self.assertTrue(html.endswith("</html>"))
+
+#     def test_home_page_returns_correct_html(self):
+#         response = self.client.get("/")
+#         self.assertContains(response, "<title>To-Do lists</title>")
+
+
+# Final test version
+
 
 class HomePageTest(TestCase):
     """Test the home page"""
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
